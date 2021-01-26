22f4fdcb681104b50b4f1c92996e8eb05fef6c5d
MinhPV <wh.pvminh@gmail.com>
2021-01-26 17:35:51 +0700

    add comment

---
+++ Server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+var mangChat=[]
+
+// socket.adapter.rooms Show danh sach room dang co
+io.on("connection", function(socket){
+  console.log("Co nguoi ket noi " + socket.id);
+
+  socket.on("tao-room", function(data){
+    socket.join(data);
+    socket.Phong=data;
+
+// send to all
+        // io.sockets.emit
+        // send to slave
+        // socket.boardcast.emit
+
+        // emit to socket id specifi
+        // io.to("socketid").emit()
+
+// join room
+    // socket.join(data)
+    // send in room
+    // io.sockets.in("ANDROID").emit("php")
+
+    var mang=[];
+    for(r in socket.adapter.rooms){
+      mang.push(r);
+    }
+    io.sockets.emit("server-send-rooms", mang);
+    socket.emit("server-send-room-socket", data);
+
+  });
+
+  socket.on("user-chat", function(data){
+    io.sockets.in(socket.Phong).emit("server-chat", data);
+  });
+
+});
+
+app.get("/", function(req, res){
+  res.render("trangchu");
+});
